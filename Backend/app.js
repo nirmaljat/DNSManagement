fa4f8dde2bd99b8410d4b7babcaf617783d0a520
Nirmal Choudhary <81221646+nirmaljat@users.noreply.github.com>
2024-05-19 13:33:39 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-   origin:'http://localhost:5173',
+   origin:'http://dns-management.vercel.app',
    methods:["GET","POST","PUT","DELETE"],
    credentials:true
  }));
